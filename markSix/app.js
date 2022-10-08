c894bd1cdf5d098d58556615c1485fdc4aba5b94
Mayank <pruthimayank2003@gmail.com>
2022-10-08 15:30:57 +0530

    markSix added

---
+++ markSix/app.js
@@ -0,0 +1,28 @@
+var btnTranslate = document.querySelector(".btn-translate");
+var txtInput = document.querySelector(".txt-input")
+var txtOutput = document.querySelector(".txt-output")
+
+var serverurl = "https://api.funtranslations.com/translate/minion.json"
+
+
+function errorHandler(error){
+    alert("Something went wrong! Try again later")
+}
+
+function translatedURL(text){
+    return serverurl + "?" + "text=" + text
+}
+
+function clickHandler(){ 
+    var inputText = txtInput.value;
+    
+    fetch(translatedURL(inputText))
+        .then(response => response.json())
+        .then(json => {
+            var translatedText = json.contents.translated
+            txtOutput.innerText = translatedText
+        })
+        .catch(errorHandler)
+}
+
+btnTranslate.addEventListener("click", clickHandler)
